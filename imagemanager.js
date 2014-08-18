2c5af6889524dcea2245f644128d588d9063d6d1
Frank Bessou <fbessou@free.fr>
2014-08-18 23:01:35 +0200

    Added listener features on imageManager

--- imagemanager.js
+++ imagemanager.js
@@ -2,25 +2,28 @@ var ImageManager = Class.extend({
 	init : function(callback){
 		this.imagedata = {};
 		this.callback = callback;
+		this.listeners = [];
 		
 		this.stillLoading = 0;
 		
-		this.load("error.png","error");
+		var errorImg = new Image();
+		errorImg.onerror = function(){console.error("ImageManager : Error image couldn't be loaded");}
+		errorImg.src = "error.png"
+		this.imagedata["error"] = errorImg; 
 	},
 	load : function(src,name){
 		var self = this;
+		var name = name || src;//todo remove .ext from src
 		
 		this.stillLoading++;
 		
 		var img = new Image();
-		img.src = src;
-		img.onload = function(){self.onLoad(img)};
 		img.onerror = function(){
-			console.warn('ImageManager : Image "'+name+'" could not be loaded.');
-			self.imagedata[name || src] = self.getImage("error");
-			self.onLoad();
+			console.warn('ImageManager : Image "'+src+'" could not be loaded.');
+			self.onLoad(name,false,self.getImage("error"));
 		}; 
-		this.imagedata[name || src] = img; //todo remove .ext from src
+		img.onload = function(){self.onLoad(name,true,img)};
+		img.src = src;
 	},
 	getImage : function(name){
 		if(!this.imagedata[name]){
@@ -29,13 +32,31 @@ var ImageManager = Class.extend({
 		}
 		return this.imagedata[name];
 	},
-	onLoad : function(){
+	onLoad : function(name, success, imgRef){
 		this.stillLoading--;
+		this.listeners.forEach(
+				function(listener){
+					if(typeof listener.onImageLoad === 'function' )
+						listener.onImageLoad(name,success,imgRef);
+				});
+
+		//add the image to the list
+		this.imagedata[name] = imgRef; 
+
 		if(!this.isLoading()){
 			this.callback();
 		}
 	},
 	isLoading : function(){
 		return !!this.stillLoading;
+	},
+	addListener : function(listener){
+		this.listeners.push(listener);
+	},
+	//removeListener can't be called inside a handler for ImageManager
+	removeListener : function(listener){
+		if(typeof listener !== 'number')
+		listener = this.listeners.indexOf(listener);
+		this.listeners.splice(listener);
 	}
 });
